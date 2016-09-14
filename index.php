18d906201cff49c8e0bc8fa9a6e98103607f961a
Sam Abrams <samabrams625@gmail.com>
2016-09-14 16:59:05 -0700

    initializing portfolio repository

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<head>
+    <title>Sam Abrams Web Development</title>
+    <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<header class="col-xs-12">
+    <h3 class="col-xs-4">Sam Abrams Web Development</h3>
+    <ul class="nav col-xs-7">
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>About Me</li>
+    </ul>
+</header>
+<div class="mainContent col-xs-12">
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: sam
+     * Date: 9/12/16
+     * Time: 9:41 AM
+     */
+    ?>
+</div>
+<footer class="col-xs-12">
+    <ul class="contactList col-xs-10">
+        <li>samabrams625@gmail.com</li>
+        <li>(909) 667-6566</li>
+        <li><a href="https://www.linkedin.com/in/samabrams223">LinkedIn</a></li>
+        <li><a href="https://twitter.com/SamAbrams23">Twitter</a></li>
+        <li><a href="https://github.com/samabrams">GitHub</a></li>
+        <li><a href="https://angel.co/sam-abrams-1">AngelList</a></li>
+    </ul>
+</footer>
+</body>
